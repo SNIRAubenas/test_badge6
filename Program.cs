2d00d882f7061e49ba74cd2e546997b6df1c6dc7
robin parlier <robin.parlier@lyceeastier.com>
2025-03-10 14:13:17 +0100

    gestion de onboard led

--- Program.cs
+++ Program.cs
@@ -7,11 +7,18 @@ class Program
     static void Main(string[] args)
     {
         RFID rfid = new RFID();
-        bool autorize; 
+        bool autorize;
+        DigitalOutput led = new DigitalOutput();        //on board led
+        DigitalOutput clanche = new DigitalOutput();    //impultion clanche
 
         //lecture badge
         rfid.Tag += (sender, e) =>
         {
+            //allumer on board led
+            led.Channel = 2;
+            led.Open(1000);
+            led.State = true;
+
             //reconnaitre si un badge est autorisé
             Console.WriteLine($"Badge : {e.Tag}");
             if (e.Tag == "1000e14339" || e.Tag == "3800637172")//tag renseigné dans la bdd
@@ -24,22 +31,24 @@ class Program
                 autorize = false;
             }
 
+            led.State = false;
+
             try
             {
 
                 if (autorize == true)
                 {
                     //ouvrir la porte, activer le contacte de la clanche
-                    DigitalOutput output = new DigitalOutput();
-                    output.Channel = 0;
-                    output.Open(1000);
-                    output.State = true;
+                    
+                    clanche.Channel = 1;
+                    clanche.Open(1000);
+                    clanche.State = true;
                     Console.WriteLine("porte ouverte");
-                    System.Threading.Thread.Sleep(5000);
-                    output.State = false;
+                    System.Threading.Thread.Sleep(2000);
+                    clanche.State = false;
                     Console.WriteLine("clanche fermée");
                     
-                    output.Close();
+                    clanche.Close();
                 }
             }
             catch (Exception ex)
